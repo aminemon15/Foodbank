4314266a79812f6a59d9f7178e9fd46713659d43
aminemon15 <aminsyifak15@gmail.com>
2019-04-12 15:15:51 +0700

    Update (Komputer VIP)

---
+++ app/src/main/java/id/gresikdev/foodbank/welcome_first.java
@@ -0,0 +1,13 @@
+package id.gresikdev.foodbank;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class welcome_first extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_first);
+    }
+}
